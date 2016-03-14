804ca2ba6cecaf2a57909d1438684b7db5cb9a03
Henri Sara <hesara@vaadin.com>
2016-03-15 01:03:31 +0200

    Rename source level parameter to use "auto" by default (#19677)
    Otherwise, using Java 8 will break GWT compilation unless the parameter
    is given explicitly.
    
    Change-Id: I638e54de2289e1456384e6b8d3bc1e09920df00e

--- src/main/java/org/codehaus/mojo/gwt/shell/CompileMojo.java
+++ src/main/java/org/codehaus/mojo/gwt/shell/CompileMojo.java
@@ -324,7 +324,7 @@ public class CompileMojo
      *
      * @since 2.6.0-rc1
      */
-    @Parameter(defaultValue = "auto", property = "maven.compiler.source")
+    @Parameter(defaultValue = "auto", property = "gwt.compiler.source")
     private String sourceLevel;
 
     /**
