f151eb53ef3f6e439873da130b544a5dd4515c9e
Teemu Suo-Anttila <tsuoanttila@users.noreply.github.com>
2016-12-20 15:53:22 +0200

    Enable Travis build (#80)
    This patch removes the old gwt-maven-plugin travis build configuration replacing it with a new one.

--- ci/deploy-snapshot.sh
+++
@@ -1,7 +0,0 @@
-if [ "$TRAVIS_REPO_SLUG" == "gwt-maven-plugin/gwt-maven-plugin" ] && \
-   [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && \
-   [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
-   [ "$TRAVIS_BRANCH" == "master" ]; then
-
-  mvn -s ci/settings.xml deploy -DskipTests
-fi
