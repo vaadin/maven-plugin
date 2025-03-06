f9e0ca0876c2173d92aa9d948814c5bd65e8a159
thevaadinman <patrik@vaadin.com>
2025-03-06 15:57:36 +0200

    Update more dependencies
    11 vulnerable dependencies remaining

--- src/main/java/org/codehaus/mojo/gwt/webxml/GwtWebInfProcessor.java
+++ src/main/java/org/codehaus/mojo/gwt/webxml/GwtWebInfProcessor.java
@@ -28,16 +28,16 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.codehaus.plexus.util.WriterFactory;
-import org.jdom.Comment;
-import org.jdom.Content;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.JDOMException;
-import org.jdom.Namespace;
-import org.jdom.input.SAXBuilder;
-import org.jdom.output.Format;
-import org.jdom.output.XMLOutputter;
-import org.jdom.xpath.XPath;
+import org.jdom2.Comment;
+import org.jdom2.Content;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.Namespace;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+import org.jdom2.xpath.XPath;
 
 /**
  * @version $Id$
